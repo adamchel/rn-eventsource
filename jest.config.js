09240dfd6dcd3dba6e47c772cb3b83477638df51
Adam Chelminski <adam.chelminski@mongodb.com>
2020-01-30 15:36:15 +0100

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    'testRegex': 'src/__tests__/.*-test\\.js$',
+    'testPathIgnorePatterns': [
+      '/node_modules/',
+    ],
+    'testEnvironment': 'node', 
+};
