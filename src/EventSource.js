7a04d9a989d280e7c63dfa40e620dec1c85cb053
Thorin0ak <philippe.camus@imail2.org>
2022-02-24 12:30:03 -0700

    Initial commit

--- src/EventSource.js
+++ src/EventSource.js
@@ -1,22 +1,22 @@
 /**
  * Copyright (c) 2020 Adam Chelminski
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to 
- * deal in the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
- * sell copies of the Software, and to permit persons to whom the Software is 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in 
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  *
  * @format
@@ -38,7 +38,7 @@ const cr = 13;
 const maxRetryAttempts = 5;
 /**
  * An RCTNetworking-based implementation of the EventSource web standard.
- *
+ *¶
  * See https://developer.mozilla.org/en-US/docs/Web/API/EventSource
  *     https://html.spec.whatwg.org/multipage/server-sent-events.html
  *     https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events
@@ -164,7 +164,8 @@ class EventSource extends (EventTarget(...EVENT_SOURCE_EVENTS): any) {
     }
 
     if (this._lastEventId) {
-      this._headers['Last-Event-ID'] = this._lastEventId;
+      // this._headers['Last-Event-ID'] = this._lastEventId;
+      this._headers = Object.assign({}, this._headers, {'LastEvent-ID': this._lastEventId});
     }
 
     Networking.sendRequest(
